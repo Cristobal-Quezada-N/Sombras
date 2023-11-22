35ad0a3eb92ccb8d3af92c85bdf2d1ff9d38de68
Cristobal Quezada <cristobal.quezada.n@usach.cl>
2023-11-22 09:19:54 -0300

    Add: Agregar comentarios descriptivos y agregar la API de Shademap

--- src/server.py
+++ src/server.py
@@ -1,18 +1,28 @@
-from flask import Flask, request, jsonify
-from simulator.map import mapa_inicializar
-APP = Flask(__name__)
+from flask import Flask, request
+from simulator.map import mapa_constructor
+from folium.elements import JavascriptLink
 
+# Instanciar el servidor APP
+app = Flask(__name__)
 
-@APP.route("/", methods=['POST', 'GET'])
+# Instanciar el mapa
+mapa = mapa_constructor()
+## Incorporar la api de Shademap 
+SHADEMAP_LIB = 'https://unpkg.com/leaflet-shadow-simulator/dist/leaflet-shadow-simulator.umd.min.js'
+mapa.get_root().html.add_child(JavascriptLink(SHADEMAP_LIB))
+
+# Crear ruta principal de la Pagina
+# - GET para entregar el mapa
+# - POST para recibir datos como coordenadas
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    mapa = mapa_inicializar()
-    coordenadas = str(request.get_data())
+    coordenadas = str(request.get_data()) # coordenadas: b'LatLng(<float>, <float>)'
     coordenadas = coordenadas[9:-2].split(', ')
     print(f"[i] Coordenadas: {coordenadas}")
+    # Servir al usuario el mapa en formato html
     return mapa.get_root().render()
 
 
+# Ejecutar el servidor si se ejecuta el archivo (server.py)
 if __name__ == "__main__":
-    APP.run(debug=True)
-
-
+    app.run(debug=True)
--- src/simulator/map.py
+++ src/simulator/map.py
@@ -1,35 +1,45 @@
 import folium
 from jinja2 import Template
-def mapa_inicializar():
+
+
+def mapa_constructor(location=[-33.4477, -70.685585], zoom=16):
          # Mapa
-         mapa_principal = folium.Map(
-             location=(-33.4400082,-70.6811497),
-             zoom_start=1000
-         )
-         # Elementos del Mapa
-         _templateClickForMarker = """{% macro script(this, kwargs) %}
-             mapa = {{this._parent.get_name()}}
-             mapa.on('click', onClick);
-             function onClick(e){
-                 if (typeof marcador != 'undefined'){
-                     marcador.setLatLng(e.latlng)
-                 }
-                 else{
-                     marcador = L.marker().setLatLng(e.latlng).addTo(mapa)
-                 }
-                 history.replaceState({}, "", `?latlng=${marcador._latlng.lat},${marcador._latlng.lng}`)
-                 fetch(
-                    '/',
-                    {
-                        method: 'POST',
-                        body: marcador._latlng
-                    },
-                 )
-             }
-         {% endmacro %}"""
-         
-         folium.ClickForMarker._template = Template(_templateClickForMarker)
-         folium.ClickForMarker().add_to(mapa_principal)
+        mapa_principal = folium.Map(
+            location= location,
+            zoom_start=zoom
+        )
+        # Extender la funcionalidad del Marcador de Mapa
+        _templateClickForMarker = """
+        {% macro script(this, kwargs) %}
+            mapa = {{this._parent.get_name()}}
+            mapa.on('click', onClick);
+            function onClick(e){
 
-         return mapa_principal
+            // Actualizar la posición del marcador
 
+               if (typeof marcador != 'undefined'){
+                   marcador.setLatLng(e.latlng)
+               }
+               else{
+                   marcador = L.marker().setLatLng(e.latlng).addTo(mapa)
+               }
+
+               // Actualizar parametros en url
+               history.replaceState({}, "", `?latlng=${marcador._latlng.lat},${marcador._latlng.lng}`)
+
+               // Comunicar las coordenadas al Servidor
+               fetch(
+                  '/',
+                  {
+                      method: 'POST',
+                      body: marcador._latlng
+                  },
+               )
+            }
+        {% endmacro %}
+        """
+
+        folium.ClickForMarker._template = Template(_templateClickForMarker)
+        folium.ClickForMarker().add_to(mapa_principal)
+
+        return mapa_principal
