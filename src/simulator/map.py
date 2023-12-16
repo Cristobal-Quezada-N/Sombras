910da5ae9e309f21329845805e76b5f407c8f4e6
Cristobal Quezada <cristobal.quezada.n@usach.cl>
2023-12-15 23:37:51 -0300

    [Edit] Modificar el formato en el cual se entrega el renderizado del mapa

--- src/simulator/map.py
+++ src/simulator/map.py
@@ -1,12 +1,15 @@
 import folium
 from jinja2 import Template
+from branca import element as Elementos
 
 
 def mapa_constructor(location=[-33.4477, -70.685585], zoom=16):
          # Mapa
         mapa_principal = folium.Map(
             location= location,
-            zoom_start=zoom
+            zoom_start=zoom,
+            height=600,
+            width=1900,
         )
         # Extender la funcionalidad del Marcador de Mapa
         _templateClickForMarker = """
@@ -27,7 +30,7 @@ def mapa_constructor(location=[-33.4477, -70.685585], zoom=16):
                // Actualizar parametros en url
                history.replaceState({}, "", `?latlng=${marcador._latlng.lat},${marcador._latlng.lng}`)
 
-               // Comunicar las coordenadas al Servidor
+            // Comunicar las coordenadas al Servidor
                fetch(
                   '/',
                   {
@@ -42,4 +45,12 @@ def mapa_constructor(location=[-33.4477, -70.685585], zoom=16):
         folium.ClickForMarker._template = Template(_templateClickForMarker)
         folium.ClickForMarker().add_to(mapa_principal)
 
-        return mapa_principal
+
+        # Seoarar los componentes del mapa
+        mapa_principal.get_root().render()
+        header = mapa_principal.get_root().header.render()
+        body   = mapa_principal.get_root().html.render()
+        script = mapa_principal.get_root().script.render()
+
+
+        return [header, body, script]
