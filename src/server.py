94661b176d480b0c5eefc083660a6831e064b1b1
Cristobal Quezada <cristobal.quezada.n@usach.cl>
2023-12-17 20:04:08 -0300

    [Edit]: Formatear la salida de mensajes internos del servidor.

--- src/server.py
+++ src/server.py
@@ -23,9 +23,9 @@ mapa = mapa_constructor()
 def index():
     if request.method == 'POST':
         coordenadas = request.get_json() # coordenadas: { 'lat' : <float>, 'lng': <float> }
-        print('\n----------------------------------------------------------------------}\n')
+        print('\n----------------------------------------------------------------------\n')
         print(f"[i] Coordenadas: {coordenadas}")
-        print('\n----------------------------------------------------------------------}\n')
+        print('\n----------------------------------------------------------------------\n')
         info = info_sol(coordenadas)
         socketio.emit('info', { 'data': info })
     return pagina_template(mapa)
@@ -45,18 +45,18 @@ def pagina_template(mapa):
 @socketio.on('connect')
 def conexion():
     # global thread
-    print('\n----------------------------------------------------------------------}\n')
+    print('\n----------------------------------------------------------------------\n')
     print('[i] Cliente Conectado')
-    print('\n----------------------------------------------------------------------}\n')
+    print('\n----------------------------------------------------------------------\n')
 # [TODO]: gestionar ubicacion
 #    with thread_detectado:
 #        if thread is None:
 #            thread = socketio.start_background_task(pedir_ubicacion)
 
 def disconnect():
-    print('\n----------------------------------------------------------------------}\n')
+    print('\n----------------------------------------------------------------------\n')
     print('[i] Client disconnected')
-    print('\n----------------------------------------------------------------------}\n')
+    print('\n----------------------------------------------------------------------\n')
 
 
 # Ejecutar el servidor si se ejecuta el archivo (server.py)
