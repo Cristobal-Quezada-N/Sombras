5dd9b72e0581e961ce7bca8b504c41bafa4dbae3
Cristobal Quezada <cristobal.quezada.n@usach.cl>
2023-12-17 19:41:00 -0300

    [Add]: Agregar una plantilla HTML para la pagina (templates/index.html).

--- src/server.py
+++ src/server.py
@@ -1,40 +1,10 @@
-from flask import Flask, request, render_template_string, request 
+from flask import Flask, request, render_template, request 
 
 from simulator.map import mapa_constructor
 from simulator.sombra import info_sol
 
 from flask_socketio import SocketIO
 # from threading import Lock
-
-template = """
-        <!DOCTYPE html>
-        <html>
-            <head>
-                {{ headers | safe }}
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.7.2/socket.io.min.js" integrity="sha384-mZLF4UVrpi/QTWPA7BjNPEnkIfRFn4ZEO3Qt/HFklTJBj/gBOV8G3HcKn4NfQblz" crossorigin="anonymous"></script>
-                <script>
-                    $(document).ready( function () {
-                        console.log("SOCKET")
-                        var socket = io.connect()
-                        socket.on('info', function(msg){
-                            //console.log("[i] Informacion recibida: " + msg.data)
-                            document.getElementById("INFO").innerHTML = msg.data;
-                        })
-                    })
-                </script>
-            </head>
-            <body>
-                <h1>USACH INFORMACIÓN SOLAR</h1>
-                {{ mapa | safe }}
-                <div style="display: block;" id="INFO">Haz click en el mapa </div>
-                {{ info | safe }}
-               <script>
-                    {{ scripts|safe }}
-               </script>
-            </body>
-        </html>
-"""
 
 # Instanciar el servidor y manejar hilos para conexiones
 app = Flask(__name__)
@@ -60,13 +30,12 @@ def index():
 
 
 def pagina_template(mapa):
-    return render_template_string(
-        source  =template,
-        # Elementos que se agregan al html
-        headers =mapa['headers'],
-        scripts =mapa['scripts'],
-        mapa    =mapa['body'],
-    )
+    return render_template('index.html',
+                           # Elementos que se agregan al html
+                           headers =mapa['headers'],
+                           scripts =mapa['scripts'],
+                           mapa    =mapa['body']
+                           )
 
 
 
@@ -80,11 +49,9 @@ def conexion():
 #        if thread is None:
 #            thread = socketio.start_background_task(pedir_ubicacion)
 
-
-
-
 def disconnect():
     print('[i] Client disconnected')
+
 
 # Ejecutar el servidor si se ejecuta el archivo (server.py)
 if __name__ == "__main__":
